91244d0d1be8595b2c99b279cfd7fa66f3b7dbfc
Jesus M Rodriguez Castro <53617197+Jesus-Rodriguez-Ca@users.noreply.github.com>
2020-02-24 15:11:26 -0800

    Add files via upload

---
+++ isPowerOfTwo.java
@@ -0,0 +1,30 @@
+// Jesus Rodriguez
+// ITC 115
+// MidTerm
+// 2/24/2020
+
+/*Write a static method isPowerOfTwo that takes an integer n as argument
+ * and that returns true if a power of two, and otherwise false. If n is zero or negative, return false. 
+ * Note that isPowerOfTwo(1) should return true, since 2^0=1.
+ */
+public class isPowerOfTwo {
+
+	public static void main(String[] args) {
+		isPowerofTwo(0);
+
+	}
+	public static void isPowerofTwo(int n) {
+		
+	
+	 if( n % 2 == 0 && n<=1) {
+		 System.out.print("True");
+	 
+	
+	}else {
+		System.out.print("False");
+	
+	
+		
+	}
+}
+}
